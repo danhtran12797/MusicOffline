f3cc1cfe98da28757d8a7f1c45aa1fa6ef667547
Danh97 <danhtran12797@gmail.com>
2019-11-26 16:56:45 +0700

    hello

--- app/src/main/java/com/danhtran12797/thd/musicoffline/OfflineActivity.java
+++ app/src/main/java/com/danhtran12797/thd/musicoffline/OfflineActivity.java
@@ -2,6 +2,7 @@ package com.danhtran12797.thd.musicoffline;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -25,8 +26,11 @@ import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -154,8 +158,29 @@ public class OfflineActivity extends AppCompatActivity implements View.OnClickLi
 
         viewPagerAdapter.registerDataSetObserver(indicator.getDataSetObserver());
         mediaPlayer = new MediaPlayer();
+    }
 
-//        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                musicAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return true;
     }
 
     private void eventButton() {
